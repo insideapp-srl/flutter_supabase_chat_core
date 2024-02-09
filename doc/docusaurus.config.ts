1244f00b2256e6079c4d2a1c11cbb1b526ef7d5c
riccardocucia <riccardocucia@gmial.com>
2024-02-09 19:37:00 +0100

    Fixed CI

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://insideapp-srl.github.io',
+    url: 'https://flutter-supabase-chat-core.insideapp.it',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/flutter_supabase_chat_core/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
